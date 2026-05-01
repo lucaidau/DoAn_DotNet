866d797112432ffc25715aa8b6b13b34b8f1a6ce
lucaidau <lucaidau@gmail.com>
2026-05-01 14:46:59 +0700

    Refactor DB: fix constraints, add triggers, clean comments, sync EF model

--- DB/DB/sp/Auth/sp_DangKi.sql
+++ DB/DB/sp/Auth/sp_DangKi.sql
@@ -1,37 +1,49 @@
-CREATE PROCEDURE sp_DangKi
-	@fullName NVARCHAR(50),
-	@userName VarChar(50),
-	@phoneNumber VarChar(10),
-	@email Char(100),
-	@gender BIT,
-	@hashPass Char(255),
-	@role BIT
+CREATE OR ALTER PROCEDURE sp_DangKi
+    @fullName   NVARCHAR(100),
+    @userName   VARCHAR(50),
+    @phoneNumber VARCHAR(10),
+    @email      VARCHAR(100),
+    @gender     BIT,
+    @hashPass   VARCHAR(100),
+    @role       BIT
 AS
-	BEGIN
-		BEGIN TRY
-			BEGIN TRANSACTION
-				IF EXISTS (SELECT 1 FROM TAI_KHOAN WHERE @userName = TenTK AND @hashPass = HashMK)
-					BEGIN
-						ROLLBACK TRANSACTION
-						SELECT 0 AS Result;
-						RETURN;
-					END
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        BEGIN TRANSACTION
 
-				INSERT INTO TAI_KHOAN(TenTK, HashMK, SDT, HoTen, GioiTinh, Email) VALUES 
-				(@userName, @hashPass, @phoneNumber, @fullName, @gender, @email)
+            IF EXISTS (SELECT 1 FROM TAI_KHOAN WHERE TenTK = @userName)
+            BEGIN
+                ROLLBACK TRANSACTION;
+                SELECT 0 AS Result;
+                RETURN;
+            END
 
-				DECLARE @NewID INT = SCOPE_IDENTITY();
+            INSERT INTO TAI_KHOAN (TenTK, HashMK, SDT, HoTen, GioiTinh, Email)
+            VALUES (@userName, @hashPass, @phoneNumber, @fullName, @gender, @email);
 
-				IF(@role = 1)
-					INSERT INTO THU_THU(IDTaiKhoan) VALUES(@NewID)
-				ELSE
-					INSERT INTO DOC_GIA (IDTaiKhoan) VALUES (@NewID)
-				COMMIT TRANSACTION
-				SELECT 1 AS Result;
-		END TRY
+            DECLARE @NewID INT = SCOPE_IDENTITY();
 
-		BEGIN CATCH
-			IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
-			SELECT -1 AS Result;
-		END CATCH
-	END
+            IF (@role = 1)
+            BEGIN
+                INSERT INTO THU_THU (IDTaiKhoan) VALUES (@NewID);
+            END
+            ELSE
+            BEGIN
+                INSERT INTO DOC_GIA (IDTaiKhoan) VALUES (@NewID);
+
+                DECLARE @NewDocGiaID INT = SCOPE_IDENTITY();
+
+                INSERT INTO THE_MUON (IDDocGia, NgayCap, NgayHetHan, TrangThai)
+                VALUES (@NewDocGiaID, GETDATE(), DATEADD(YEAR, 1, GETDATE()), 1);
+            END
+
+        COMMIT TRANSACTION;
+        SELECT 1 AS Result;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+        SELECT -1 AS Result;
+    END CATCH
+END
+GO
